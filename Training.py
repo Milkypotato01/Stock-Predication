c5a88ef129906bcd3b6eb25824016c32204a0773
Beast09 <atharav.ingale09@gmail.com>
2026-02-16 00:54:26 +0530

    Accuracy achieved 65%

--- Training.py
+++ Training.py
@@ -1,44 +1,24 @@
 from sklearn.ensemble import RandomForestClassifier
 from Preprocessing import proper_preprocessing
 from Data_spliting import test_train_divide
-import pandas as pd
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, roc_auc_score
+import joblib
 
 df = proper_preprocessing(r"C:\Users\dell\OneDrive\Desktop\Stock predictation Project\test data\indexProcessed.csv")
 X_train, X_test, y_train, y_test = test_train_divide(df)
 
 model = RandomForestClassifier(
     n_estimators=200,
-    max_depth=None,
-    min_samples_split=2,
+    max_depth=10,
+    min_samples_split=10,
+    min_samples_leaf=5,
+    class_weight="balanced",
     random_state=42
 )
 
 print("Training the model...")
 
 model.fit(X_train, y_train)
-y_pred = model.predict(X_test)
-y_prob = model.predict_proba(X_test)[:, 1]  # Get probabilities for the positive class
+joblib.dump(model, "stock_model.pkl")
 
-def model_evaluation(y_test, y_pred, y_prob):
-    print("Accuracy:", accuracy_score(y_test, y_pred))
-    print("ROC-AUC:", roc_auc_score(y_test, y_prob))
-    print(confusion_matrix(y_test, y_pred))
-    print(classification_report(y_test, y_pred))
-    
-    feature_importance = pd.Series(
-        model.feature_importances_,
-        index=X_train.columns
-    ).sort_values(ascending=False)
-    
-    print(feature_importance)
-    
-    
-    baseline = [1] * len(y_test)
-    print("Baseline Accuracy:", accuracy_score(y_test, baseline))
-    
-    print("Train accuracy:", model.score(X_train, y_train))
-    print("Test accuracy:", model.score(X_test, y_test))
+print("Model saved as stock_model.pkl")
 
-print("Evaluating the model...")
-model_evaluation(y_test, y_pred, y_prob)
